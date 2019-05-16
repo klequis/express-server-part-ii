384d86d2f3e7901717c66cb93a99b7255865b0db
klequis <klequis@gmail.com>
2019-05-16 10:27:27 -0700

    initial from where minimal-server left off

---
+++ build-server.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+npm i -D fs-extra rimraf
+
+# REPODIR=$PWD
+
+# DESTDIR=~/dev/modern-fullstack-subproj/app
+
+
+# DESTDIR='.'
+# echo ${DESTDIR}
+
+
+# npm i
+
+# if [ -d "$DESTDIR" ]; then
+  # rm -rf $DESTDIR
+# fi
+
+# mkdir $DESTDIR
+
+# npm run build
+# mv $REPODIR/dist/* $DESTDIR
