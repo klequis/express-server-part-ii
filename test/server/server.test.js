c3ffb63a00b58f745a6a6d030761cd159a701899
klequis <klequis@gmail.com>
2019-05-23 20:08:52 -0700

    -wip

--- test/server/server.test.js
+++ test/server/server.test.js
@@ -1,5 +1,6 @@
 import { expect } from "chai";
 import request from 'supertest'
+import { objectIdFromHexString } from 'db'
 import { fourTodos, oneTodo } from "./fixture"
 import app from 'server'
 import {
@@ -40,6 +41,19 @@ describe('todo-route', function() {
     })
   })
 
+  describe('test GET /api/todo/:id', function() {
+    let _idToGet = ''
+    before(async function() {
+      await dropCollection(collectionName)
+      const insert = await insertMany(collectionName, fourTodos)
+      console.log('insert', )
+      _idToGet = insert.data[1]._id
+    })
+    it('should get todo with specified _id', async function() {
+
+    })
+  })
+
   describe('test POST /api/todo', function() {
     before(async function () {
       await dropCollection(collectionName)
@@ -52,18 +66,38 @@ describe('todo-route', function() {
         .expect('Content-Type', /json/)
         .expect(200)
       const data = post.body.data
-      console.log('post', data)
       expect(data.title).to.equal(oneTodo.title)
       expect(data.completed).to.equal(false)
 
       // Confirm using find(). Remember, find() will return an array
       const findRes = await find(collectionName, {})
-      console.log('findRes', findRes)
       const findData = findRes.data
-      console.log('findData', findData)
       expect(findData[0].title).to.equal(oneTodo.title)
       expect(findData[0].completed).to.equal(false)
       
+    })
+  })
+
+  describe('test DELETE /api/todo/:id', function() {
+    before(async function() {
+      await dropCollection(collectionName)
+      await insertMany(collectionName, fourTodos)
+    })
+    let _idToDelete = ''
+    it('check there are 4 records', async function() {
+      const findRes = await find(collectionName, {})
+      expect(findRes.data.length).to.equal(4)
+      _idToDelete = findRes.data[1]._id
+    })
+    it('should delete one record', async function() {
+      const post = await request(app)
+        .delete(`/api/todo/${_idToDelete}`)
+        .set('Accept', 'application/json')
+        .send()
+        .expect(200)
+      const deleteData = post.body.data
+      console.log('deleteData', deleteData)
+      expect(deleteData._id).to.equal(getObjectId(_idToDelete))
     })
   })
 })
