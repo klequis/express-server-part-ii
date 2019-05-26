a60cc1ae127debf76c2705b19e30b9529125f5a1
klequis <klequis@gmail.com>
2019-05-25 17:02:07 -0700

    add/update isValidHexString. Remove unused functions.

--- db/helpers.js
+++ db/helpers.js
@@ -1,57 +1,23 @@
 import { ObjectID } from  'mongodb'
 import { omit } from 'ramda'
 
-export const objectIdFromHexString = async (hexId) => {
+// export const removeIdProp = (obj) => {
+//   return omit(['_id'], obj)
+// }
+
+const checkForHexString = new RegExp('^[0-9a-fA-F]{24}$')
+
+export const isValidHexIdString = id => {
+  let _id = ''
   try {
-    return await ObjectID.createFromHexString(hexId)
-  }
-  catch (e) {
-    // console.error('error ---')
-    console.error('ERROR /db/helpers.js.objectidFromHexString', e)
-  }
-}
-
-export const getObjectId = async (id) => {
-
-  // const valid = ObjectID.isValid(id)
-  // console.log('valid', valid)
-  if (ObjectID.isValid(id)) {
-    const objId = await objectIdFromHexString(id)
-    // console.log('objId', typeof objId)
-
-    return objId
-  } else {
-    throw new Error('this is an error')
+    if (!id) {
+      return false
+    } else {
+      typeof id !== 'string' ? (_id = id.toString()) : (_id = id)
+    }
+    return checkForHexString.test(_id)
+  } catch (e) {
+    red(e)
+    return false
   }
 }
-
-export const removeIdProp = (obj) => {
-  return omit(['_id'], obj)
-}
-
-
-// console.log('getObjectId - good', getObjectId('5ccb0df578d66d21de7ae4a1'))
-// console.log('getObjectId - bad', getObjectId('abc'))
-// objectIdFromHexString('abc')
-
-const isValid = (id)  => {
-  console.log('isValid', ObjectID.isValid(id))
-}
-
-// isValid('5ccb0df578d66d21de7ae4a1')
-// isValid('abc')
-
-
-const tryIt = (id) => {
-  try {
-    const a = ObjectID.createFromHexString(id)
-    // console.log('a', a)
-  }
-  catch (e) {
-    // console.log('ERROR: tryIt', e)
-    throw e
-  }
-}
-
-
-// tryIt('abc')
